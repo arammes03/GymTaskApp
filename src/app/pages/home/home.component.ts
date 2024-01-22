eb369ed9aa0b0eae3e2458f5265f1f76e5e657aa
arammes03 <arammes03@gmail.com>
2024-01-22 10:45:35 +0100

    Inserción y borrado de tasks

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,4 +10,22 @@ import { Component, signal } from '@angular/core';
 })
 export class HomeComponent {
   tasks = signal(['Ir al gym', 'Sacar 100kg en banca', 'Tener 300kg en dl']);
+
+  // Handler que maneja la inserción de un nuevo task
+  newTaskInputHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+
+    // Actualizamos nuestra lista de tasks sin resetear los datos
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  // Handler que nos permite borrar un task
+  deleteTaskHandler(index: number) {
+    /* Actualizamos nuestra lista filtrando por los tasks y su posición.
+    Donde la posición sea distinta al indice que queremos no se borra */
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
 }
